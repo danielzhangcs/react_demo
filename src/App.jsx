1ae2b70c9b6ec1493d1723fa9ac8960cd0b8461e
Nick Anna <he_nanna@homesite.com>
2020-09-08 13:34:48 -0400

    Add Image and Hello world text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { exampleStyle, logoStyle }  from './styles/commonStyles';
+import reactLogo from './images/logo.svg';
+
+const helloString = 'Hello World!';
+
 function App() {
   return (
-    <div>
-      Hello World!
+    <div style={exampleStyle}>
+      <img src={reactLogo} style={logoStyle} height="100" alt="logo" />
+      {helloString}
     </div>
   );
 }
---
+++ src/styles/commonStyles.js
@@ -0,0 +1,7 @@
+export const exampleStyle = {
+  fontSize: 32,
+};
+
+export const logoStyle = {
+  display: 'block',
+};
